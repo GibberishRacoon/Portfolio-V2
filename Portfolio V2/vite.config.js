fc3d257380a0d72d57a866873b38636c66da772d
s24580 <s24580@pjwstk.edu.pl>
2024-05-19 23:09:49 +0200

    vite.config fix to dev server

--- Portfolio V2/vite.config.js
+++ Portfolio V2/vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: './Portfolio V2',  
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './Portfolio V2/index.html'  
-    }
-  }
+  plugins: [react()]
 });

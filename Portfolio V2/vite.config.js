c822694cc9718ffaa09f42d713b655cadc2c139a
s24580 <s24580@pjwstk.edu.pl>
2024-05-19 22:14:14 +0200

    vercel fix

--- Portfolio V2/vite.config.js
+++ Portfolio V2/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: './portfolio-v2',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: './portfolio-v2/index.html'
+    }
   }
 });
